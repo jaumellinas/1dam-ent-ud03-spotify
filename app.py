2d6fe2dec624b57fb559a5c6b82a596f19d48b18
Jaume Llinàs <64520369+jaumellinas@users.noreply.github.com>
2025-03-08 18:25:31 +0100

    Update app.py
    Arreglamos SQLite para que funcione con Vercel (o eso creemos)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///canciones.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/canciones.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
